3111027ec2566fc4a7a68a10eb1f71a2480bb98c
virgantara <vinux.edu@gmail.com>
2017-08-22 19:36:31 +0700

    change border color

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
       <div class="pull-right hidden-xs">
         <b>Version</b> 1.0.1
       </div>
-      <strong>Copyright &copy; 2017 <a href="http://keltech.co.id">Keltech</a>.</strong> All rights
-      reserved.
+      <!-- <strong>Copyright &copy; 2017 <a href="http://keltech.co.id">Keltech</a>.</strong> All rights
+      reserved. -->
     </div>
     <!-- /.container -->
   </footer>
