089a2ea1cb1958d16cad18f53eb27f879c96799f
virgantara <vinux.edu@gmail.com>
2017-10-19 08:29:31 +0700

    add db wrapper

---
+++ ajax.php
@@ -0,0 +1,61 @@
+<?php 
+
+include_once "db_helper.php";
+require 'vendor/autoload.php';
+
+if(empty($_POST['q'])) exit;
+
+$q = $_POST['q'];
+
+$m= new MongoDB\Client(C_CONN);
+$db = $m->ahp;
+
+if($q == 'compute')
+	$collection = $db->compute;
+else if($q == 'cdn')
+	$collection = $db->cdn;
+else if($q == 'storage')
+	$collection = $db->storage;
+else if($q == 'dns')
+	$collection = $db->dns;
+else if($q == 'paas')
+	$collection = $db->paas;
+
+
+
+$documents = $collection->find([]);
+$headers = array();
+foreach($documents as $doc)
+{
+  $size = count($doc);
+  if($size > 1){
+    foreach($doc as $q=>$v)
+    {
+    	if($q != '_id')
+      $headers[] = $q;
+      
+    }
+    break;
+    
+  }
+}
+
+$documents = $collection->find([]);
+
+$content = array();
+foreach($documents as $row)
+{
+	$size = count($doc);
+  	if($size > 1){
+		$content[] = $row;
+	}
+}
+
+$results = array(
+	'headers' => $headers,
+	'content' => $content
+);
+
+echo json_encode($results);
+
+?>
